9512476c7b41fe6d16dbf1c164117db9fa44b552
lybtongji <lybtongji@users.noreply.github.com>
2022-11-01 13:50:05 +0800

    add action

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# build-intel-edison
+# Build Intel Edison RootFS Image
+
+Yocto layer: https://github.com/edison-fw/meta-intel-edison/
+
+## Login
+
+Login via serial port. default user is `root` with no password.
+
+## WiFi
+
+Configuration
+
+``` shell
+cat << 'EOF' | tee /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+ctrl_interface=/var/run/wpa_supplicant
+ctrl_interface_group=0
+update_config=1
+
+network={
+  ssid="<SSID>"
+  psk="<password>"
+  key_mgmt=WPA-PSK
+  proto=WPA2
+  pairwise=CCMP TKIP
+  group=CCMP TKIP
+  scan_ssid=1
+}
+EOF
+```
+
+``` shell
+cat << 'EOF' | tee /etc/systemd/network/25-wlan.network
+[Match]
+Name=wlan0
+
+[Network]
+DHCP=yes
+EOF
+```
+
+Connect to WiFi
+
+``` shell
+systemctl enable --now wpa_supplicant@wlan0.service
+systemctl enable --now systemd-networkd.service
+```
---
+++ distros/debian/run.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+[ -z "${DEPLOY}" ] && exit 1
+[ -z "${ROOTFS}" ] && exit 1
+[ -z "${IMAGE_EXT4}" ] && exit 1
+
+apt update
+apt install -y debootstrap
+
+rm -rf ${ROOTFS}
+mkdir -p ${ROOTFS}
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+pkgs=(
+    bash-completion
+    curl
+    dbus
+    dbus-user-session
+    openssh-server
+    policykit-1
+    sudo
+    systemd-resolved
+    systemd-timesyncd
+    vim
+    wpasupplicant
+)
+
+debootstrap \
+    --arch amd64 \
+    --merged-usr \
+    --no-check-gpg \
+    --include $(join_by , ${pkgs[*]}) \
+    stable ${ROOTFS} https://deb.debian.org/debian
+
+install -d ${ROOTFS}/boot
+install -m 0755 ${DEPLOY}/images/edison/bzImage ${ROOTFS}/boot/bzImage
+install -m 0755 ${DEPLOY}/images/edison/core-image-minimal-initramfs-edison.cpio.gz ${ROOTFS}/boot/initrd
+
+cp -r ${DEPLOY}/deb ${ROOTFS}/tmp/
+rm -v ${ROOTFS}/tmp/deb/edison/kernel-{dev,dbg}_*.deb
+
+mount -t proc /proc ${ROOTFS}/proc
+mount -t sysfs /sys ${ROOTFS}/sys
+mount -o bind /dev ${ROOTFS}/dev
+mount -o bind /dev/pts ${ROOTFS}/dev/pts
+
+chroot ${ROOTFS} bash -c "dpkg -i --force-bad-version /tmp/deb/edison/kernel-*.deb"
+chroot ${ROOTFS} bash -c "dpkg -i /tmp/deb/all/bcm43340-fw_*.deb"
+chroot ${ROOTFS} bash -c "dpkg -i /tmp/deb/corei7-64/gadget_*.deb"
+
+umount ${ROOTFS}/dev/pts
+umount ${ROOTFS}/dev
+umount ${ROOTFS}/sys
+umount ${ROOTFS}/proc
+
+rm -rf ${ROOTFS}/tmp/*
+
+echo edison > ${ROOTFS}/etc/hostname
+ln -s /lib/systemd/system/serial-getty@.service ${ROOTFS}/etc/systemd/system/multi-user.target.wants/serial-getty@ttyS2.service
+sed -i 's/^root:[^:]*/root:/' ${ROOTFS}/etc/shadow
+
+echo ROOTFS size $(du -sb ${ROOTFS} | cut -f1)
+mkfs.ext4 -d ${ROOTFS} ${IMAGE_EXT4} 1048576
